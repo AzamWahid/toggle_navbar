3f15abb1d355310146d4e0dc9cc3df48a24fc103
azam.wahid <azamwahid@outlook.com>
2024-11-16 01:50:10 +0500

    done

--- app.js
+++ app.js
@@ -10,4 +10,24 @@ function toggleMenu() {
     else {
         document.querySelector('.mobMenuItems').classList.toggle('showMobNavItems')
     }
+    // menuShow = false;
+    console.log(menuShow);
 }
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        document.querySelector('.mobMenuItems').classList.remove('showMobNavItems')
+    } else {
+    }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 769px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function () {
+    myFunction(x);
+});
